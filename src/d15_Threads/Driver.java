15458520b90e3bd355da8bf0e4caa642f430a61d
adem kok <ademk7604@gmail.com>
2023-10-23 17:00:57 +0200

    Threads

---
+++ src/d15_Threads/Driver.java
@@ -0,0 +1,52 @@
+package d15_Threads;
+
+
+class Gorev extends Thread{
+	@Override
+	public void run() {
+		for(int i=1; i<=20; i++) {
+			System.out.println("-Gorev sinifi -"+"Numara: " +i);
+		}
+	}
+}
+public class Driver {
+	/*
+	 *Thread
+	 *1. thread ler process'lerin hafif halidir.
+	 *2.Bir programin en kucuk bagimsiz birimidir.
+	 *3.Ayri bir yurutme yolu vardir.
+	 *4.Her java programi en az 1 threa icerir
+	 * 5.java.lang.Tread sinifindan bir thread olustuurlur
+	 * ve kontrol edilir.
+	 */
+	/*
+	 * Thread Lifecycle 
+	 * new->runnable<->running->terminated
+	 * running -> waitin->runnable yapilabiliyor
+	 * 
+	 * 
+	 */
+	
+	//mein metot main tread'i temsil ediyor
+	public static void main(String[] args) {
+		System.out.println("Aktif thread sayisi: "+Thread.activeCount());
+		//int sayi = 5/0;
+		//System.out.println(sayi);
+		//Exception in thread "main" java.lang.ArithmeticException: / by zero
+		
+		//JOB 1
+		System.out.println("rogram basladi");
+		
+		
+		
+		//JOB 2
+		
+		Gorev gorev = new Gorev();
+		gorev.start(); // otoratikmen run methodu calisiyor
+		
+		
+		System.out.println("Program bitti!");
+		
+	}
+
+}
